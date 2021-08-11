796c45a2550dbfa6ab23d4c7a7bed8d8c6975ae3
Arthur Herminie <arthur.herminie@gmail.com>
2021-08-11 15:12:26 +0200

    created models and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+umanota = Restaurant.create! name: "Uma Nota", address: "154 rue Saint Denis", phone_number: "063838383", category: "japanese"
+pizza_east = Restaurant.create! name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0678787878", category: "italian"
+
+# [umanota, pizza_east].each do |attributes|
+#   restaurant = Restaurant.create!(attributes)
+#   puts "Created #{restaurant.name}"
+# end
+puts "Finished!"
+
+puts "Creating reviews..."
+review = Review.create! content: "éclaté au sol", rating: 5, restaurant: umanota
+review = Review.create! content: "éclaté au sol", rating: 4, restaurant: umanota
+review = Review.create! content: "éclaté on sol", rating: 4, restaurant: umanota
+review = Review.create! content: "éclaté on sol", rating: 2, restaurant: pizza_east
+review = Review.create! content: "éclaté au sol", rating: 1, restaurant: pizza_east
+review = Review.create! content: "éclaté on sol", rating: 3, restaurant: pizza_east
+puts "Finished!"
